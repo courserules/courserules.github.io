244f221349a446eaac3915a035ac6945c948e0e3
tamfr <ejm29@case.edu>
2016-05-25 12:53:12 -0500

    trying to fix broken control

--- js/controllers.js
+++ js/controllers.js
@@ -1042,6 +1042,7 @@ myApp.controller("gMap",function($scope, Markers, Polylines, Options){
   $scope.map = { 
     center: { latitude: 27.6932, longitude: -97.2805 }, 
     zoom: 12,
+    mapTypeControl: true,
     mapTypeControlOptions: {
         style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
         position: google.maps.ControlPosition.BOTTOM_CENTER
